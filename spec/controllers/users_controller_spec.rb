5132528c156d09c7a8fd9be49b3033717f53b67d
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-08-17 14:11:21 +0100

    added tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  describe '#create' do
+    context "when details validates" do
+      it 'creates user' do
+        post :create, params: { user: attributes_for(:user) }
+        expect(User.last.firstname).to eql('Dummy')
+        expect(session["flash"]["flashes"]["success"]).
+          to eql("Registration Successful")
+      end
+    end
+  end
 end
